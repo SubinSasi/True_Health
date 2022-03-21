89b3f2d3fead27eb181fe93ebda626f8e18d23f9
Subin Sasi <subinsasi0@gmail.com>
2022-03-21 21:39:34 +0530

    js second image function

--- js/test.js
+++ js/test.js
@@ -20,3 +20,25 @@ function test1()
         document.getElementById("test-para1").innerHTML = ""
     }
 }
+// SECOND IMAGE
+function test2()
+{
+    var input = document.getElementById("test-number2").value
+    document.getElementById("test-print2").innerHTML = input
+
+    if(input == "5"){
+        document.getElementById("test-print2").innerHTML = "The number which you entered is RIGHT"
+    }else document.getElementById("test-print2").innerHTML = "The number which you entered is WRONG" 
+    
+    if(input == "5"){
+        document.getElementById("test-para2").innerHTML = "Most color blind people won't see this number clearly"
+    }else document.getElementById("test-para2").innerHTML = "Image showned  number is 5"
+
+    if(input == ""){
+        document.getElementById("test-print2").innerHTML = "OOPS!!! Please Enter any number"
+    }
+
+    if(input == ""){
+        document.getElementById("test-para2").innerHTML = ""
+    }
+}
